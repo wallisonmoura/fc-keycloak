dabf98e3c8b158df9e0286128146d8555177da67
wallisonmoura <wallison_cedaspy@hotmail.com>
2024-11-14 02:12:52 -0300

    Creating Keycloak login redirects

--- authentication-flow/implicit-flow/src/App.tsx
+++ authentication-flow/implicit-flow/src/App.tsx
@@ -1,9 +1,31 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Login } from "./Login";
+import { Logout } from "./Logout";
+import { Callback } from "./Callback";
+
+const router = createBrowserRouter([
+  {
+    path: "login",
+    element: <Login />,
+  },
+  {
+    path: "logout",
+    element: <Logout />,
+  },
+  {
+    path: "admin",
+    element: <div>admin</div>,
+  },
+  {
+    path: "callback",
+    element: <Callback />,
+  },
+]);
+
 function App() {
 
   return (
-    <div>
-      <h1>Implicit Flow</h1>
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ authentication-flow/implicit-flow/src/Login.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react"
+import { Navigate } from "react-router-dom"
+import { makeLoginUrl } from "./utils";
+
+export function Login() {
+  const authContext = {
+    auth: true
+  }
+
+  useEffect(() => {
+    if(!authContext.auth) {
+      window.location.href = makeLoginUrl();
+    }
+
+  }, [authContext])
+
+  return authContext.auth ? <Navigate to="/admin" /> : <div>Loading</div>
+}
