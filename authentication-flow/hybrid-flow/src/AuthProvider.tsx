027fc49eaee442fc12827f235d621bf1edeadb6b
wallisonmoura <wallison_cedaspy@hotmail.com>
2024-11-16 16:48:58 -0300

    implementing hybrid flow

---
+++ authentication-flow/hybrid-flow/src/AuthProvider.tsx
@@ -0,0 +1,67 @@
+import { PropsWithChildren, createContext, useCallback, useState } from "react"
+import * as utils from "./utils"
+import { JWTPayload } from "jose"
+
+type AuthContextProps = {
+  auth: JWTPayload | null
+  makeLoginUrl: () => string
+  makeLogoutUrl: () => string | false
+  login: (
+    accessToken: string,
+    idToken: string,
+    code: string,
+    state: string
+  ) => JWTPayload
+};
+
+const initContextData: AuthContextProps = {
+  auth: null,
+  makeLoginUrl: utils.makeLoginUrl,
+  //@ts-expect-error - this is a mock function
+   
+  makeLogoutUrl: () => {},
+  //@ts-expect-error - this is a mock function
+   
+  login: () => {},
+};
+
+//create a context for the login state
+// eslint-disable-next-line react-refresh/only-export-components
+export const AuthContext = createContext(initContextData)
+
+//create a provider for the login state
+export const AuthProvider = (props: PropsWithChildren) => {
+  const makeLogin = useCallback(
+    (accessToken: string, idToken: string, code: string, state: string) => {
+      //@ts-expect-error - for refresh token param
+      const authData = utils.login(accessToken, idToken, null, state)
+      setData((oldData) => ({
+        auth: authData,
+        makeLoginUrl: oldData.makeLoginUrl,
+        makeLogoutUrl: oldData.makeLogoutUrl,
+        login: oldData.login,
+      }))
+      utils.exchangeCodeForToken(code).then((authData) => {
+        setData((oldData) => ({
+          auth: authData,
+          makeLoginUrl: oldData.makeLoginUrl,
+          makeLogoutUrl: oldData.makeLogoutUrl,
+          login: oldData.login,
+        }))
+      })
+      return authData;
+    },
+    []
+  )
+
+  const [data, setData] = useState({
+    auth: utils.getAuth(),
+    makeLoginUrl: utils.makeLoginUrl,
+    makeLogoutUrl: utils.makeLogoutUrl,
+    login: makeLogin,
+  })
+
+  return (
+    <AuthContext.Provider value={data}>{props.children}</AuthContext.Provider>
+  )
+}
